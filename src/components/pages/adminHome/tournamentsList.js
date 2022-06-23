3878249fb14ee6d7f23369f5ced5437ed7d28dfc
Batyrbek <bkkb2016@gmail.com>
2022-06-23 17:44:47 +0600

    updated

--- src/components/pages/adminHome/tournamentsList.js
+++ src/components/pages/adminHome/tournamentsList.js
@@ -1,13 +1,39 @@
+import style from "./tournamentList.module.css";
+import { useState, useEffect } from "react";
+import fetchTournaments from "../../services/tournaments";
+import SetMap from "../map/map";
+
 const TournamentList = (props) => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const res = await fetchTournaments();
+      setData(res);
+    }
+    getData();
+  }, [data]);
+
   return (
     <div>
-      <h1>Tournaments</h1>
-      <div>
+      <h1 className={style.header}>Tournaments</h1>
+      <div className={style.tournamentList}>
         {props.tournamentData.map(({ tournament }) => (
-          <div style={{ color: "white" }} key={tournament.id}>
-            ID: {tournament.id} || tournament name: {tournament.name} ||
-            tournament description {tournament.description} || tournament type{" "}
-            {tournament.tournament_type}
+          <div
+            onClick={() => {
+              props.handleMapClick(tournament.url);
+            }}
+            className={style.individualTournament}
+            key={tournament.id}
+          >
+            <span>ID: {tournament.id}</span>
+            <br></br>
+            <span>tournament name: {tournament.name}</span>
+            <br></br>
+            <span>tournament description {tournament.description}</span>{" "}
+            <br></br>
+            <span>tournament type {tournament.tournament_type}</span>
+            <br></br>
+            <span>tournament url {tournament.url}</span>
           </div>
         ))}
       </div>
