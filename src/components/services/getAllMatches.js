210482b0fb795d33d1bf96908ceb9d5c864560d6
Batyrbek <bkkb2016@gmail.com>
2022-06-23 23:41:14 +0600

    updated

--- src/components/services/getAllMatches.js
+++ src/components/services/getAllMatches.js
@@ -1,21 +1,24 @@
 import key from "../pages/adminHome/key";
 
-function getAllMatches(id) {
+async function getAllMatches(id) {
   const password = key;
   const tournamentID = id;
   let url = `https://api.challonge.com/v1/tournaments/${tournamentID}/matches?api_key=${password}`;
 
-  fetch(url, {
-    method: "GET",
-    headers: {
-      //headers,
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-  //.done();
+  try {
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        //headers,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default getAllMatches;
