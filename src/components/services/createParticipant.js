6c6b15e0d0e36d32cd54986cc135d4dcb083e666
Batyrbek <bkkb2016@gmail.com>
2022-06-24 10:13:46 +0600

    updated

--- src/components/services/createParticipant.js
+++ src/components/services/createParticipant.js
@@ -1,4 +1,42 @@
+// import key from "../pages/adminHome/key";
+
+// function createParticipant(tournamentID, participantName, participantEmail) {
+//   console.log("test from createParticipant");
+//   console.log(tournamentID, participantName, participantEmail);
+//   let participant = {
+//     name: participantName,
+//     email: participantEmail,
+//   };
+//   console.log(participant);
+//   PostCreateParticipant(tournamentID, participant);
+// }
+
+// async function PostCreateParticipant(tournamentID, participant) {
+//   const password = key;
+//   let url = `https://api.challonge.com/v1/tournaments/${tournamentID}/participants.json`;
+//   console.log(tournamentID, participant);
+//   try {
+//     await fetch(url, {
+//       method: "POST",
+//       headers: {
+//         //headers,
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ api_key: password, participant: participant }),
+//     })
+//       .then((response) => response.json())
+//       .then((json) => console.log(json));
+//   } catch (error) {
+//     // console.log(error);
+//     alert(error);
+//   }
+// }
+
+// export default createParticipant;
+
 import key from "../pages/adminHome/key";
+import getParticipants from "./getParticipants";
 
 function createParticipant(tournamentID, participantName, participantEmail) {
   console.log("test from createParticipant");
@@ -16,7 +54,7 @@ async function PostCreateParticipant(tournamentID, participant) {
   let url = `https://api.challonge.com/v1/tournaments/${tournamentID}/participants.json`;
   console.log(tournamentID, participant);
   try {
-    await fetch(url, {
+    const res = await fetch(url, {
       method: "POST",
       headers: {
         //headers,
@@ -24,12 +62,23 @@ async function PostCreateParticipant(tournamentID, participant) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ api_key: password, participant: participant }),
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+    });
+    const data = await res.json;
+    console.log("test data");
+    console.log(data);
+    console.log("test participant name");
+    console.log(participant.name);
+    const dataTour = await getParticipants(tournamentID);
+    console.log(dataTour);
+    const participantID = dataTour.find((participant1) => {
+      return participant1.participant.name === participant.name;
+    });
+    console.log(participantID);
+    alert(`Your uniquie participant id is ${participantID.participant.id}`);
+    //.then((response) => alert(`Your ID in tournament`)
   } catch (error) {
-    // console.log(error);
-    alert(error);
+    console.log(error);
+    //alert(error);
   }
 }
 
